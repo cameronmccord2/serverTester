e2672077f6b4ce10ec78d7ca626a00478ba08830
Cameron McCord <cameronmccord2@gmail.com>
2013-07-07 10:27:55 -0600

    added tester controller

---
+++ src/testerCtrl.js
@@ -0,0 +1,23 @@
+function TesterCtrl($rootScope, $scope, $location, $routeParams, $http){
+	var serverPath = "http://localhost:8909/"
+	var user = {
+		firstName:'',
+		lastName:'',
+		username:'',
+		password:'',
+		email:''
+	};
+	var login = {
+		username:'',
+		password:''
+	};
+
+	var sequence = new Object();
+	sequence.login = function(){
+		$http.get(serverPath + 'map/user/login', query).success(function(data){
+			$scope.login = data;
+		}).error(function(data, status, headers, config){
+			console.log("error logging in", data, status, headers, config);
+		})
+	}
+}
