120afcf730c2cafe44442a34387f0a8c8dda6a9e
Cameron McCord <cameronmccord2@gmail.com>
2013-07-07 10:55:48 -0600

    added tester template

--- src/testerCtrl.js
+++ src/testerCtrl.js
@@ -16,6 +16,7 @@ function TesterCtrl($rootScope, $scope, $location, $routeParams, $http){
 	sequence.login = function(){
 		$http.get(serverPath + 'map/user/login', query).success(function(data){
 			$scope.login = data;
+			$scope.loginString = angular.toJson(data);
 		}).error(function(data, status, headers, config){
 			console.log("error logging in", data, status, headers, config);
 		})
