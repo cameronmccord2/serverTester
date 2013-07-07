5444fba64c077ba9e6a4cac61e9e907c0da85296
Cameron McCord <cameronmccord2@gmail.com>
2013-07-07 12:00:50 -0600

    basic hand-input tests for new user, login, doesUsernameExist are done.

--- src/testerCtrl.js
+++ src/testerCtrl.js
@@ -1,24 +1,62 @@
 function TesterCtrl($rootScope, $scope, $location, $routeParams, $http){
-	var serverPath = "http://localhost:8909/"
-	var user = {
+	var serverPath = "http://localhost:8909/";
+	$scope.input = new Object();
+	$scope.newUser = {
 		firstName:'',
 		lastName:'',
 		username:'',
 		password:'',
 		email:''
 	};
-	var login = {
+	$scope.login = {
 		username:'',
 		password:''
 	};
-
-	var sequence = new Object();
-	sequence.login = function(){
-		$http.get(serverPath + 'map/user/login', query).success(function(data){
+	$scope.sequence = new Object();
+	$scope.sequence.login = function(username, password){
+		console.log("trying to login")
+		var config = {
+			params:{
+				username: username,
+				password: password
+			}
+		};
+		$http.get(serverPath + 'map1/user/login', config).success(function(data){
 			$scope.login = data;
-			$scope.loginString = angular.toJson(data);
+			$scope.loginString = data.token.length;
+			console.log(data);
 		}).error(function(data, status, headers, config){
+			if(status == 0)
+				console.log("server not there: " + serverPath);
 			console.log("error logging in", data, status, headers, config);
-		})
+		});
+	}
+	$scope.sequence.checkUsername = function(username){
+		if(username == '')
+			return;
+		var config = {
+			params:{
+				username: username
+			}
+		};
+		$http.get(serverPath + 'map1/doesUsernameExist', config).success(function(data){
+			if(data == 'doesntExist')
+				$scope.input.usernameGood = true;
+			else if(data == 'exists')
+				$scope.input.usernameGood = false;
+		}).error(function(data, status, headers, config){
+			
+		});
+	}
+	$scope.sequence.newUser = function(){
+		var config = {
+			params:$scope.newUser
+		}
+		$http.get(serverPath + 'map1/user/new', config).success(function(data){
+			$scope.newUser.string = "success";
+		}).error(function(data, status, headers, config){
+			console.log("error setting up new user", data, status, headers, config);
+			$scope.newUser.string = "failure" + angular.toJson(data);
+		});
 	}
 }
