904a84f51068b73e38e23ab82c2d188783c506fd
Cameron McCord <cameronmccord2@gmail.com>
2013-07-10 20:19:20 -0600

    got testing working for some endpoints

--- src/testerCtrl.js
+++ src/testerCtrl.js
@@ -1,5 +1,10 @@
 function TesterCtrl($rootScope, $scope, $location, $routeParams, $http, $q){
 	var serverPath = "http://localhost:8909/";
+	$scope.stopTest = false;
+	$scope.testStatuses = {
+		good:'good',
+		bad:'bad'
+	};
 	$scope.input = new Object();
 	$scope.newUser = {
 		firstName:'',
@@ -12,57 +17,41 @@ function TesterCtrl($rootScope, $scope, $location, $routeParams, $http, $q){
 		username:'',
 		password:''
 	};
-	$scope.tests = new Array();
 	$scope.sequence = new Object();
-	$scope.sequence.login = function(username, password){
-		console.log("trying to login")
-		var config = {
-			params:{
-				username: username,
-				password: password
-			}
-		};
-		$http.get(serverPath + 'map1/user/login', config).success(function(data){
-			$scope.login = data;
-			$scope.loginString = data.token.length;
-			console.log(data);
-		}).error(function(data, status, headers, config){
-			if(status == 0)
-				console.log("server not there: " + serverPath);
-			console.log("error logging in", data, status, headers, config);
-		});
+	$scope.resultsArray = new Array();
+	var call = {
+		get:function(path, params){
+			console.log("here", path, params)
+			var config = undefined;
+			if(params != undefined)
+				config = {
+					params:params
+				};
+			var defer = $q.defer();
+			$http.get(path, config).success(function(data, status, headers, config){
+				defer.resolve({data:data, status:status, headers:headers, config:config});
+			}).error(function(data, status, headers, config){
+				defer.resolve({data:data, status:status, headers:headers, config:config});
+			});
+			return defer.promise;
+		}
 	}
-	$scope.sequence.checkUsername = function(username){
-		if(username == '')
-			return;
-		var config = {
-			params:{
-				username: username
-			}
-		};
-		$http.get(serverPath + 'map1/doesUsernameExist', config).success(function(data){
-			if(data == 'doesntExist')
-				$scope.input.usernameGood = true;
-			else if(data == 'exists')
-				$scope.input.usernameGood = false;
-		}).error(function(data, status, headers, config){
-			
-		});
+
+	$scope.sequence.login = function(newLogin){
+		return call.get(serverPath + 'map1/user/login', newLogin);
+	}
+	$scope.sequence.checkUsername = function(usernameObject){
+		return call.get(serverPath + 'map1/doesUsernameExist', usernameObject);
 	}
 	$scope.sequence.newUser = function(newUser){
-		var config = {
-			params:newUser
-		}
-		var defer = $q.defer();
-		$http.get(serverPath + 'map1/user/new', config).success(function(data, status, headers, config){
-			defer.resolve({data:data, status:status, headers:headers, config:config});
-		}).error(function(data, status, headers, config){
-			console.log("newuser", data, status, headers, config);
-			defer.resolve({data:data, status:status, headers:headers, config:config});
-		});
-		return defer.promise;
+		return call.get(serverPath + 'map1/user/new', newUser);
 	}
-	var testResult = function(result, valid, data, status, headers, config){
+	$scope.sequence.userData = function(token){
+		return call.get(serverPath + 'map/user/data', token);
+	}
+
+
+	var evaluateResult = function(result, valid, data, status, headers, config){
 		var resultString = '';
 		var extra = false;
 		if(data != undefined){
@@ -93,24 +82,86 @@ function TesterCtrl($rootScope, $scope, $location, $routeParams, $http, $q){
 		return {resultString:resultString, result:result, expected:{data:data, status:status, headers:headers, config:config}};
 
 	}
-	$scope.tests[0] = {
-		name:"Create User",
-		tests:[
-			function(){
-				var defer = $q.defer();
-				$scope.sequence.newUser().then(function(result){
-					var testResult = testResult(result, 'Valid', 'Missing token: firstName', 401);
-					if(testResult != 'Valid')
-						console.log(testResult);
-					defer.resolve(testResult);
-				});
-
-				return defer.promise;
-			},
-			function(){
-
+	var runTest = function(sequence, category, testTitle, inputData, data, status, headers, config){
+		console.log(sequence, category, testTitle, inputData, data, status, headers, config)
+		var defer = $q.defer();
+		var validResult = "Valid";
+		inputData.token = $scope.currentToken;
+		console.log(inputData)
+		$scope.sequence[sequence](inputData).then(function(result){
+			var testResult = evaluateResult(result, validResult, data, status, headers, config);
+			if(result.data.token != undefined){
+				$scope.currentToken = result.data.token;
+				console.log("saved current token", result.data.token)
 			}
-		]
+			var statusClass = $scope.testStatuses.bad;
+			if(testResult.resultString == validResult)
+				statusClass = $scope.testStatuses.good;
+			console.log({testStatus:statusClass, sequence:sequence, category:category, testTitle:testTitle, inputData:inputData, result:result, data:data, status:status, headers:headers, config:config});
+			$scope.resultsArray.push({testStatus:statusClass, sequence:sequence, category:category, testTitle:testTitle, inputData:inputData, result:result, data:data, status:status, headers:headers, config:config});
+			defer.resolve(testResult);
+		});
+		return defer.promise;
 	}
-	$scope.tests[0].tests[0]();
+
+	$scope.testLoop = function(testIndex){
+		var buildString = '';
+		$scope.currentDefer = $q.defer();
+		for (var i = 0; i < $scope.tests[testIndex].tests.length; i++) {
+			console.log("hit loop")
+			if($scope.stopTest){
+				$scope.stopTest = false;
+				break;
+			}else{
+				//eval('runTest($scope.tests["' + testIndex + '"].sequence, $scope.tests["' + testIndex + '"].category, $scope.tests["' + testIndex + '"].tests["' + i + '"][0], $scope.tests["' + testIndex + '"].tests["' + i + '"][1], $scope.tests["' + testIndex + '"].tests["' + i + '"][2], $scope.tests["' + testIndex + '"].tests["' + i + '"][3], $scope.tests["' + testIndex + '"].tests["' + i + '"][4]);');
+				buildString += 'runTest($scope.tests["' + testIndex + '"].sequence, $scope.tests["' + testIndex + '"].category, $scope.tests["' + testIndex + '"].tests["' + i + '"][0], $scope.tests["' + testIndex + '"].tests["' + i + '"][1], $scope.tests["' + testIndex + '"].tests["' + i + '"][2], $scope.tests["' + testIndex + '"].tests["' + i + '"][3], $scope.tests["' + testIndex + '"].tests["' + i + '"][4])';
+				if(i != $scope.tests[testIndex].tests.length - 1)
+					buildString += '.then(function(){';
+				else{
+					buildString += '.then(function(){$scope.currentDefer.resolve()';
+					for (var i = 0; i < $scope.tests[testIndex].tests.length; i++) {
+						buildString += ';})';
+					}
+				}
+			}
+		};
+		console.log(buildString + ';');
+		eval(buildString + ';');
+		return $scope.currentDefer.promise;
+	}
+
+	$scope.tests = [
+		{
+			category:"Create User",
+			sequence:'newUser',
+			tests:[
+				['No Data', {}, 'Missing token: firstName', 401],
+				['First Name', {firstName:"cam"}, 'Missing token: lastName', 401],
+				['Last Name', {firstName:"cam", lastName:"McCord"}, 'Missing token: email', 401],
+				['Username', {firstName:"cam", lastName:"McCord", username:"username"}, 'Missing token: email', 401],
+				['Password', {firstName:"cam", lastName:"McCord", username:"username", password:"password"}, 'Missing token: email', 401],
+				['Valid User', {firstName:"cam", lastName:"McCord", username:"username", password:"password", email:"cameronmccord2@gmail.com"}, undefined, 200],
+				['Existing User', {firstName:"cam", lastName:"McCord", username:"username", password:"password", email:"cameronmccord2@gmail.com"}, "Username already exists", 401]
+			]
+		},
+		{
+			category:"Login",
+			sequence:"login",
+			tests:[
+				['No Data', {}, 'Missing token: username', 401],
+				['Username', {username:"stuff"}, 'Missing token: password', 401],
+				['Good Login', {username:"username", password:"password"}, undefined, 200]
+			]
+		},
+		{
+			category:"User Data",
+			sequence:"userData",
+			tests:[
+				['No Data', {}, 'Missing authorization token', 401],
+				['With token', {token:$scope.currentToken}, undefined, 200]
+			]
+		}
+	];
+	$scope.testLoop(0).then(function(){$scope.testLoop(1).then(function(){$scope.testLoop(2);});});
+
 }
